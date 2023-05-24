d0315b0acf2f1a27be632be5b377508cf91f2b28
icynic <101461427+icynic@users.noreply.github.com>
2023-05-24 15:15:53 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,24 +3,24 @@ import base64
 import json
 
 # --- Required information --- #
-token ="YOUR-TOKEN"
-user="USER-NAME"
-repository="REPOSITORY-NAME"
-online_path="UPLOAD-DIRECTORY"
-message="a new commit message"
-local_file_path="FILE-TO-UPLOAD"
+token = "YOUR-TOKEN"
+user = "USER-NAME"
+repository = "REPOSITORY-NAME"
+online_path = "UPLOAD-DIRECTORY"  # Make sure to place a slash at the end if it is not empty
+message = "a new commit message"
+local_file_path = "FILE-TO-UPLOAD"
 
 
 # Open the local file (as bytes), get its name and content
 with open(local_file_path, "rb+") as file:
-    file_name=file.name
-    file_content=file.read()
+    file_name = file.name
+    file_content = file.read()
 
     # Form the URL, header
-    url = f"https://api.github.com/repos/{user}/{repository}/contents/{online_path}/" + file_name
+    url = f"https://api.github.com/repos/{user}/{repository}/contents/{online_path}" + file_name
     headers = {"Authorization": "token " + token}
     # Encode the file content in base64, then convert it to a string
-    content=base64.b64encode(file_content).decode('utf-8')
+    content = base64.b64encode(file_content).decode('utf-8')
     # Form the data to upload
     data = {
         "message": message,
@@ -36,4 +36,3 @@ with open(local_file_path, "rb+") as file:
     print(re_data['content']['html_url'])
 
 # icynic
-
